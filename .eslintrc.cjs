750141cf6212dcc4bf3dbd3aff6c2f60595535b5
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-26 01:24:35 +0900

    feat: cypress 로그인 test 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    'cypress/globals': true  // Cypress 전역 변수 추가
+    'cypress/globals': true, // Cypress 전역 변수 추가
   },
   extends: [
     'eslint:recommended',
@@ -13,13 +13,13 @@ module.exports = {
     'prettier',
     'plugin:storybook/recommended',
     'plugin:tailwindcss/recommended',
-    'plugin:cypress/recommended'  // Cypress 권장 설정 추가
+    'plugin:cypress/recommended', // Cypress 권장 설정 추가
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: [
-    'react', 
+    'react',
     'react-refresh',
-    'cypress'  // Cypress 플러그인 추가
+    'cypress', // Cypress 플러그인 추가
   ],
 };
