1964a9ee403ffb37745c747db1b551236278f6eb
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-04-26 01:23:42 +0900

    feat: cypress 로그인 테스트 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    'cypress/globals': true  // Cypress 전역 변수 추가
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -9,8 +13,13 @@ module.exports = {
     'prettier',
     'plugin:storybook/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:cypress/recommended'  // Cypress 권장 설정 추가
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'react-refresh'],
+  plugins: [
+    'react', 
+    'react-refresh',
+    'cypress'  // Cypress 플러그인 추가
+  ],
 };
--- cypress/e2e/home/disco.cy.js
+++ cypress/e2e/home/disco.cy.js
@@ -1,7 +1,9 @@
 describe('example counter app', () => {
   beforeEach(() => {
     cy.visit('https://www.disco.re/');
+    cy.get('.disco-welcome-close-icon').click();
     cy.get('#menu_btn').click();
+    
   });
 
   it('로그인/로그아웃', () => {
@@ -11,18 +13,21 @@ describe('example counter app', () => {
     //when (사용자가 동작했을때)
     //then(이렇게 동작할것이다)
 
-    if (cy.get('.sidebar-profile-name').should('have.value', '')) {
-      cy.get('#sidebar_open_login_btn')
-        .click()
-        .then(() => {
-          cy.get('#login_email').type('@@@@@');
-          cy.get('#login_password').type('@@@');
-          cy.wait(500);
-          cy.get('.member-entry-submit-btn').click({
-            multiple: true,
-            force: true,
-          });
+    if (cy.get('#sidebar_open_login_btn').should('have.value', '')) {
+        // 첫 번째 명령 체인: 로그인 버튼을 클릭하기 위한 조건 확인
+        cy.get('.sidebar-profile-name').should('have.value', '').then((profileName) => {
+            if (profileName) {  // 이 조건은 사실상 항상 참이므로, 로직을 확인해야 함
+            // eslint-disable-next-line cypress/unsafe-to-chain-command
+            cy.get('#sidebar_open_login_btn').click().then(() => {
+                cy.get('#login_email').type('testjalee2651@gmail.com');
+                cy.get('#login_password').type('gkdgo123!');
+                // eslint-disable-next-line cypress/no-unnecessary-waiting
+                cy.wait(500); // wait 사용은 가능한 피하는 것이 좋습니다. 대신 요소가 준비될 때까지 기다리는 로직 사용
+                cy.get('.member-entry-submit-btn').click({ multiple: true, force: true });
+            });
+            }
         });
+  
     } else {
       cy.get('#sidebar_logout_btn').click();
     }
