9d82ca29cae9758721af3deaedeb1c86de4d1e90
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-05-01 23:02:55 +0900

    fix: vitest수정

--- src/utils/__test__/authUtils.test.ts
+++ src/utils/__test__/authUtils.test.ts
@@ -1,7 +1,7 @@
 import { isValidEmail, isValidPassword } from '../authUtils';
 
 describe('이메일 검증 유틸리티 함수 테스트', () => {
-  const emailValidator = new isValidEmail();  // 인스턴스 생성
+  const emailValidator = new isValidEmail(); // 인스턴스 생성
 
   it('이메일이 유효할 경우 true 를 반환한다.', () => {
     expect(emailValidator.validate('test@test.com')).toBe(true);
@@ -15,7 +15,7 @@ describe('이메일 검증 유틸리티 함수 테스트', () => {
 });
 
 describe('비밀번호 검증 유틸리티 함수 테스트', () => {
-  const passwordValidator = new isValidPassword();  // 인스턴스 생성
+  const passwordValidator = new isValidPassword(); // 인스턴스 생성
 
   it('비밀번호가 6자리 이상이 아닐 경우 false 를 반환한다.', () => {
     expect(passwordValidator.validate('some')).toBe(false);
