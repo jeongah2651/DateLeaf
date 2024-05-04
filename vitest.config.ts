835f564647443b475b221613e35a54d7d2c43127
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-05-04 17:26:01 +0900

    fix: 오류해결

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   resolve: {
     alias: {
-      '@': './src'
-    }
+      '@': './src',
+    },
   },
   test: {
     environment: 'jsdom',
